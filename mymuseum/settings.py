dd48d7a7e6816edc0f9f7249f2be2e6a419eb5dd
jatoad <jasperhmca.todd@gmail.com>
2023-07-03 13:34:30 +0000

    added heroku deploy link to ALLOWED_HOSTS

--- mymuseum/settings.py
+++ mymuseum/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mymuseum.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mymuseum.herokuapp.com', 'localhost', 'mymuseum-2cb032376ab5.herokuapp.com']
 
 
 # Application definition
