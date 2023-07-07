d43359a1fd5b97b52a1902e3d446180959074e2a
jatoad <jasperhmca.todd@gmail.com>
2023-07-07 09:39:55 +0000

    created and migrated sepcimen class model

--- museum/models.py
+++ museum/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+# from django.contrib.auth.models import user
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Specimen(models.Model):
+    species_name = models.CharField(max_length=200)
+    specimen_image = CloudinaryField('image', default='placeholder')
+    date_found = models.CharField(max_length=200)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=200)
+    additional_information = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Meta:
+    ordering = ['-upload_date']
+
+
+def __str__(self):
+    return self.species_name
+
+
+
+
