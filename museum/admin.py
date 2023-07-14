187851d94f72bfe02a4ff72e431b863f1228d254
jatoad <jasperhmca.todd@gmail.com>
2023-07-14 11:45:30 +0000

    added filter, display list, search fields to admin section fro comments and specimen

--- museum/admin.py
+++ museum/admin.py
@@ -7,9 +7,14 @@ from django_summernote.admin import SummernoteModelAdmin
 class SpecimenAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('additional_information')
+    list_filter = ('species_name', 'location', 'when_found', 'author', 'upload_date')
+    list_display = ('species_name', 'upload_date',)
+    search_fields = ['species_name', 'location', 'author']
 
 
 @admin.register(Comment)
 class CommentsAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('comment_content')
+    list_display = ('name', 'comment_content', 'specimen', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
