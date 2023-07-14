613e12280c7b8a2d64f9faacb00daa69f75c7a12
jatoad <jasperhmca.todd@gmail.com>
2023-07-14 10:33:37 +0000

    added comment class to models

--- museum/admin.py
+++ museum/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Specimen
+from .models import Specimen, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Specimen)
 
-# Register your models here.
+@admin.register(Specimen)
+class SpecimenAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('additional_information')
+
+
+@admin.register(Comment)
+class CommentsAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('comment_content')
--- museum/models.py
+++ museum/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
@@ -7,9 +7,11 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 class Specimen(models.Model):
     species_name = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='specimen_upload', default=True)
     specimen_image = CloudinaryField('image', default='placeholder')
-    date_found = models.CharField(max_length=200)
+    when_found = models.CharField(max_length=200)
     upload_date = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     location = models.CharField(max_length=200)
     additional_information = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
@@ -23,5 +25,18 @@ def __str__(self):
     return self.species_name
 
 
+class Comment(models.Model):
+    specimen = models.ForeignKey(Specimen, on_delete=models.CASCADE, related_name='Comment')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    comment_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
 
+class Meta:
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    return f"{self.name} commented: {self.comment_content}"
