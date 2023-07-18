d7ba2bbdb9550a3ca3bf520bc72042376e18f041
jatoad <jasperhmca.todd@gmail.com>
2023-07-18 11:52:49 +0000

    added views code for specimens

--- museum/views.py
+++ museum/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Specimen, Comment
 
-# Create your views here.
+
+class SpecimenList(generic.ListView):
+    model = Specimen
+    queryset = Specimen.objects.filter(status=1).order_by(-created_on)
+    template_name = 'index.html '
+
