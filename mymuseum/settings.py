6871d8fa566e4e3df6cc393a23c30e8fb2cec5a3
jatoad <jasperhmca.todd@gmail.com>
2023-07-26 08:51:05 +0000

    added allauth user login

--- mymuseum/settings.py
+++ mymuseum/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-    'alluth',
+    'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary_storage',
