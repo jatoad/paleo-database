4b45df523a300fcb87568ca3570f69963f684e2c
jatoad <jasperhmca.todd@gmail.com>
2023-11-19 12:45:03 +0000

    More updates for vercel deployment take 2

--- mymuseum/settings.py
+++ mymuseum/settings.py
@@ -160,9 +160,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"), "templates"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
