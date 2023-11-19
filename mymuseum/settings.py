24d47247ef00f4509657ee5f68a51ab4ce9c14a4
jatoad <jasperhmca.todd@gmail.com>
2023-11-19 10:52:59 +0000

    added static to urls

--- mymuseum/settings.py
+++ mymuseum/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
